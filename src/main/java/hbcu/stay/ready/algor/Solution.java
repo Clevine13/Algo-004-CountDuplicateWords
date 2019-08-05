3ba85c1e75a33dc81c6114e539bdc9d933fa8ff0
Carl Levine Jr <clevine313@gmail.com>
2019-08-05 09:45:32 -0400

    not much done

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,22 @@
 package hbcu.stay.ready.algor;
 
+import java.util.Collection;
+import java.util.HashMap;
+
 public class Solution {
 
     public String countUniqueWords(String input){
-        return null;
+        int count = 0;
+        HashMap<String,Integer> newmap = new HashMap<>();
+
+        String [] output = input.split(" ");
+
+        for(int i = 0; i < output.length; i++){
+
+            if(newmap.containsKey(output[i])){
+                count ++;
+            }
+        }
+            return null;
     }
 }
